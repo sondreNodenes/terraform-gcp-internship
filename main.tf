767abb8bd2c3b8210509e271779d5074373d094f
sondrenodenesNTNU1 <sondrnod@stud.ntnu.no>
2025-10-07 13:23:48 +0200

    formattet tf files

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ terraform {
 
   required_providers {
     google = {
-        source = "hashicorp/google"
-        version = "~> 5.0"
+      source  = "hashicorp/google"
+      version = "~> 5.0"
     }
   }
 }
@@ -13,81 +13,81 @@ terraform {
 #GCP provider, reuse variables from variables.tf
 provider "google" {
   project = var.project_id
-  region = var.region
-  zone = var.zone
+  region  = var.region
+  zone    = var.zone
 }
 
-data "google_compute_image" "debian"{
-    family = "debian-12"
-    project = "debian-cloud"
-} 
+data "google_compute_image" "debian" {
+  family  = "debian-12"
+  project = "debian-cloud"
+}
 
 #VM instance
 resource "google_compute_instance" "vm" {
-    name = var.instance_name
-    machine_type = var.machine_type
-    zone = var.zone
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
 
-    allow_stopping_for_update = true
+  allow_stopping_for_update = true
 
 
-    # Tag for firewall rules 
-    tags = ["intern-assignment"]
+  # Tag for firewall rules 
+  tags = ["intern-assignment"]
 
-    labels = {
-        student = var.student_name
+  labels = {
+    student = var.student_name
+  }
+
+  metadata_startup_script = file("${path.module}/startup-script.sh")
+
+  #service account for VM
+  service_account {
+    scopes = ["cloud-platform"]
+  }
+
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.debian.self_link
     }
+  }
 
-    metadata_startup_script = file("${path.module}/startup-script.sh")
+  #Network interfae with external IP
+  network_interface {
+    network = "default"
 
-    #service account for VM
-    service_account {
-      scopes = ["cloud-platform"]
+    access_config {
+      #external IP 
     }
-
-    boot_disk{
-        initialize_params{
-            image = data.google_compute_image.debian.self_link
-        }
-    }
-    
-    #Network interfae with external IP
-    network_interface {
-      network = "default"
-
-      access_config {
-        #external IP 
-      }
-    }
+  }
 }
 
 #Firewall rule to alow HTTP traffic from the whole internet (source_ranges) to intern-assignments
 resource "google_compute_firewall" "allow_htp" {
-  name = "allow-http-intern-assignment"
+  name    = "allow-http-intern-assignment"
   network = "default"
 
   allow {
     protocol = "tcp"
-    ports = ["80", "443"]
+    ports    = ["80", "443"]
   }
 
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["intern-assignment"]
+  target_tags   = ["intern-assignment"]
 }
 
 #Firewall rule to allow SSH from allowed groups/users
 resource "google_compute_firewall" "allow_iap_ssh" {
-  name = "allow-iap-ssh-intern-assignment"
+  name    = "allow-iap-ssh-intern-assignment"
   network = "default"
 
   allow {
     protocol = "tcp"
-    ports = ["22"]
+    ports    = ["22"]
 
   }
 
   source_ranges = ["35.235.240.0/20"]
-  target_tags = ["intern-assignment"]
+  target_tags   = ["intern-assignment"]
 }
 
 /*
@@ -105,6 +105,6 @@ resource "google_dns_record_set" "vm_dns" {
   type         = "A"
   ttl          = 300
   managed_zone = "sandbox4-vcops-tech"
-  
+
   rrdatas = [google_compute_instance.vm.network_interface[0].access_config[0].nat_ip]
 }
--- variables.tf
+++ variables.tf
@@ -1,39 +1,39 @@
 # GCP Project config
 variable "project_id" {
   description = "GCP project ID"
-  type = string
+  type        = string
 }
 
 # Region and Zone config 
 variable "region" {
   description = "GCP region for resources"
-  type = string
-  default = "europe-north1" #Closest to Norway
+  type        = string
+  default     = "europe-north1" #Closest to Norway
 }
 
 variable "zone" {
   description = "GCP zone for compute resources"
-  type = string
-  default = "europe-north1-a" #a in additon for zone (datacenter)
+  type        = string
+  default     = "europe-north1-a" #a in additon for zone (datacenter)
 }
 
 variable "student_name" {
   description = "Student name for labes"
-  type = string 
-  default = "sondre_nodenes"
+  type        = string
+  default     = "sondre_nodenes"
 }
 
 # VM instance config
 variable "instance_name" {
   description = "Name of the VM instance"
-  type = string 
-  default = "px-intern-vm"
+  type        = string
+  default     = "px-intern-vm"
 }
 
 variable "machine_type" {
   description = "Machine type for the VM"
-  type = string
-  default = "e2-micro" #Type as assignment says
+  type        = string
+  default     = "e2-micro" #Type as assignment says
 }
 
 

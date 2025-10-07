5587c411ed994920bbc0be4095b0a427d3fb2b2a
sondrenodenesNTNU1 <sondrnod@stud.ntnu.no>
2025-10-07 10:37:52 +0200

    added http and https URLs to outputs

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,16 @@ output "vm_external_ip" {
   value = "http://${google_compute_instance.vm.network_interface[0].access_config[0].nat_ip}"
 }
 
+output "vm_http_url" {
+  description = "HTTP URL to access the web server (redirects to HTTPS)"
+  value = "http://vm.sandbox4.vcops.tech"
+}
+
+output "vm_https_url" {
+  description = "HTTPS URL to access the web server"
+  value = "https://vm.sandbox4.vcops.tech"
+}
+
 output "vm_name" {
   description = "Name of the VM instance"
   value = google_compute_instance.vm.name
